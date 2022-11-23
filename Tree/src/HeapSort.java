5b59151af29c43c71e76bd4d288f5ebcf6981ab4
Macy Fong <macyfong525@gmail.com>
2022-11-22 19:01:02 -0800

    add heap on array based

---
+++ Tree/src/HeapSort.java
@@ -0,0 +1,56 @@
+import java.util.Arrays;
+
+public class HeapSort<E extends Comparable<E>> {
+    // reheap index starting from 0
+    public static <T extends Comparable<? super T>>
+    void heapSort(T[] array, int n) {
+        // Create first heap
+        for (int rootIndex = n / 2 - 1; rootIndex >= 0; rootIndex--)
+            reheap(array, rootIndex, n - 1);
+
+        swap(array, 0, n - 1);
+
+        for (int lastIndex = n - 2; lastIndex > 0; lastIndex--) {
+            reheap(array, 0, lastIndex);
+            swap(array, 0, lastIndex);
+        }
+    }
+
+    public static void swap(Object[] arr, int x, int y) {
+        Object temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
+    }
+
+    private static <T extends Comparable<? super T>>
+    void reheap(T[] heap, int rootIndex, int lastIndex) {
+        boolean done = false;
+        T orphan = heap[rootIndex];
+        int leftChildIndex = 2 * rootIndex + 1;
+
+        while (!done && (leftChildIndex <= lastIndex)) {
+            int largerChildIndex = leftChildIndex;
+            int rightChildIndex = leftChildIndex + 1;
+
+            if ((rightChildIndex <= lastIndex) &&
+                    heap[rightChildIndex].compareTo(heap[largerChildIndex]) > 0) {
+                largerChildIndex = rightChildIndex;
+            } // end if
+
+            if (orphan.compareTo(heap[largerChildIndex]) < 0) {
+                heap[rootIndex] = heap[largerChildIndex];
+                rootIndex = largerChildIndex;
+                leftChildIndex = 2 * rootIndex + 1;
+            } else
+                done = true;
+        }
+        heap[rootIndex] = orphan;
+    }
+
+    public static void main(String[] args) {
+        HeapSort<Integer> heap = new HeapSort<>();
+        Integer[] arr = {3, 8, 2, 0, 7};
+        heap.heapSort(arr, arr.length);
+        System.out.println(Arrays.toString(arr));
+    }
+}
---
+++ Tree/src/MinHeap.java
@@ -0,0 +1,137 @@
+import java.util.Arrays;
+
+public class MinHeap<T extends Comparable<? super T>> implements MinHeapInterface<T> {
+    // reheap index starting from 1
+    private T[] heap;      // Array of heap entries; ignore heap[0]
+    private int lastIndex; // Index of last entry and number of entries
+    private static final int DEFAULT_CAPACITY = 25;
+
+    public MinHeap(T[] entries) {
+        this(entries.length);
+        lastIndex = entries.length;
+        // Copy given array to data field
+        for (int index = 0; index < entries.length; index++)
+            heap[index + 1] = entries[index];
+        // Create heap
+        for (int rootIndex = lastIndex / 2; rootIndex > 0; rootIndex--)
+            reheap(rootIndex);
+    }
+
+    public MinHeap(int initialCapacity) {
+        if (initialCapacity < DEFAULT_CAPACITY)
+            initialCapacity = DEFAULT_CAPACITY;
+
+        // The cast is safe because the new array contains null entries
+        @SuppressWarnings("unchecked")
+        T[] tempHeap = (T[]) new Comparable[initialCapacity + 1];
+        heap = tempHeap;
+        lastIndex = 0;
+    }
+
+    public MinHeap() {
+        this(DEFAULT_CAPACITY);
+    }
+
+
+    @Override
+    public void add(T newEntry) {
+        int newIndex = lastIndex + 1;
+        int parentIndex = newIndex / 2;
+        while (parentIndex > 0 && newEntry.compareTo(heap[parentIndex]) < 0) {
+            heap[newIndex] = heap[parentIndex];
+            newIndex = parentIndex;
+            parentIndex /= 2;
+        }
+        heap[newIndex] = newEntry;
+        lastIndex++;
+        ensureCapacity();
+    }
+
+    private void ensureCapacity() {
+        int capacity = heap.length;
+        if (lastIndex >= capacity) {
+            int newCapacity = 2 * capacity;
+            heap = Arrays.copyOf(heap, newCapacity);
+        }
+    }
+
+
+    private void reheap(int rootIndex) {
+        boolean done = false;
+        T orphan = heap[rootIndex];
+        int leftChildIndex = 2 * rootIndex;
+
+        while (!done && (leftChildIndex <= lastIndex)) {
+            int largerChildIndex = leftChildIndex; // Assume larger
+            int rightChildIndex = leftChildIndex + 1;
+
+            if ((rightChildIndex <= lastIndex) &&
+                    heap[rightChildIndex].compareTo(heap[largerChildIndex]) < 0) {
+                largerChildIndex = rightChildIndex;
+            }
+
+            if (orphan.compareTo(heap[largerChildIndex]) > 0) {
+                heap[rootIndex] = heap[largerChildIndex];
+                rootIndex = largerChildIndex;
+                leftChildIndex = 2 * rootIndex;
+            } else
+                done = true;
+        }
+        heap[rootIndex] = orphan;
+    }
+
+
+    @Override
+    public T removeMin() {
+        T root = null;
+
+        if (!isEmpty()) {
+            root = heap[1];
+            heap[1] = heap[lastIndex];
+            lastIndex--;
+            reheap(1);
+        }
+
+        return root;
+
+    }
+
+    @Override
+    public T getMin() {
+        T root = null;
+        if (!isEmpty())
+            root = heap[1];
+        return root;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return lastIndex < 1;
+    }
+
+    @Override
+    public int getSize() {
+        return lastIndex;
+    }
+
+    @Override
+    public void clear() {
+        for (int i = 0; i <= lastIndex; i++) {
+            heap[i] = null;
+        }
+        lastIndex = 0;
+    }
+
+    public void print() {
+        for (int i = 1; i <= lastIndex; i++) {
+            System.out.println("Parent: " + heap[i] + " Leftchild: " + heap[i * 2] + " RightChild " + heap[i * 2 + 1]);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Integer[] arr = {3, 8, 2, 1, 7, 9, 4, 6, 5};
+        MinHeap<Integer> obj = new MinHeap<>(arr);
+        obj.print();
+    }
+}
