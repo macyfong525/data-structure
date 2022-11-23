83426db67b2d69c43b919480f073c0a34c2a89c7
Macy Fong <macyfong525@gmail.com>
2022-11-22 19:01:02 -0800

    add heap on array based

---
+++ Tree/src/MinHeapInterface.java
@@ -0,0 +1,43 @@
+public interface MinHeapInterface<T extends Comparable<? super T>> {
+    /**
+     * Adds a new entry to this heap.
+     *
+     * @param newEntry An object to be added.
+     */
+    public void add(T newEntry);
+
+    /**
+     * Removes and returns the smallest item in this heap.
+     *
+     * @return Either the smallest object in the heap or,
+     * if the heap is empty before the operation, null.
+     */
+    public T removeMin();
+
+    /**
+     * Retrieves the smallest item in this heap.
+     *
+     * @return Either the smallest object in the heap or,
+     * if the heap is empty, null.
+     */
+    public T getMin();
+
+    /**
+     * Detects whether this heap is empty.
+     *
+     * @return True if the heap is empty, or false otherwise.
+     */
+    public boolean isEmpty();
+
+    /**
+     * Gets the size of this heap.
+     *
+     * @return The number of entries currently in the heap.
+     */
+    public int getSize();
+
+    /**
+     * Removes all entries from this heap.
+     */
+    public void clear();
+}
